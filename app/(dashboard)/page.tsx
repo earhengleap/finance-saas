e9af2ca6b6950d6acb5954fb37169854846792bb
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-13 01:56:22 +0700

    Infinance

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <p>
+      This is an authenticated page.{" "}
+      <UserButton afterSignOutUrl="/" />
+    </p>
+  );
+}

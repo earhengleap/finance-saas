f906e3a029b71ce566f5320354c337e5fc3f8e16
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-14 13:21:45 +0700

    InFinance

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,23 +3,18 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import accounts  from './accounts';
 
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-.get(
-    '/hello', 
-    clerkMiddleware(),
-    (c) => {
-        return c.json({
-            hello: 'world'})
-    }
-);
-
+const routes = app
+    .route("/accounts", accounts)
 
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes;

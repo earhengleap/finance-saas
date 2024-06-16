822edc960e9e83a88ac18b24fc41fe059a66306f
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-16 14:33:29 +0700

    Infinance

--- features/components/new-accounts-sheet.tsx
+++ features/components/new-accounts-sheet.tsx
@@ -1,21 +1,50 @@
 import { useNewAccount } from "@/features/hooks/use-new-account";
-import { Sheet, SheetContent, SheetTrigger, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { AccountForm } from "@/components/account-form";
+import { useCreateAccount } from "@/features/api/use-create-account";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
 
-    const { isOpen, onClose } = useNewAccount();
-    return (
-        <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>
-                        New Account
-                    </SheetTitle>
-                    <SheetDescription>
-                        Create a new account to track your transactions.
-                    </SheetDescription>
-                </SheetHeader>
-            </SheetContent>
-        </Sheet>
-    );
-}
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{ name: "" }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};

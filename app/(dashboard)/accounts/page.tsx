e94422da35717c0c6a3d4de88476f854358b8acb
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-17 14:43:57 +0700

    Infinance

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Loader2, Plus } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/hooks/use-new-account";
+import { useGetAccounts } from "@/features/api/user-get-account";
+
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "@/components/data-table";
+import { columns } from "./columns";
+
+const AccountPage = () => {
+  const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+                <Loader2 className={"size-6 text-slate-300 animate-spin"} />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Account Page</CardTitle>
+          <Button onClick={newAccount.onOpen} size={"sm"}>
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            onDelete={() => {}}
+            filterKey={"email"}
+            columns={columns}
+            data={accounts}
+            disabled={false}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+export default AccountPage;

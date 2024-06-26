3120dd6a86dcb2ab83cb732e27b516a0a6cdd1bc
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-26 13:23:58 +0700

    Deployment

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -4,7 +4,7 @@ const SettingsPage = () => {
   return (
     <div className="w-full">
         <div className="flex items-center justify-center text-3xl text-muted-foreground mt-20">
-            <h1>Settings is under development :)</h1>
+            <h1>Under development :)</h1>
         </div>
       <Footer />
     </div>
